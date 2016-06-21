5d599b90370d41e0b579304994778da901262e51
Victor Benavente <VMB@Victors-MacBook-Air.local>
2016-06-21 02:10:43 -0700

    added icecream controller testing

--- vic-class-24-assignment-clientsideapp/server.js
+++ vic-class-24-assignment-clientsideapp/server.js
@@ -1,6 +1,20 @@
 'use strict';
 
 const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  next();
+});
+
+app.get('/icecream', (req, res) => {
+  res.json({});
+});
+
+app.get('/milkshake', (req, res) => {
+  res.json({});
+});
 
 express().use(express.static(__dirname + '/build'))
 .listen(8080, () => console.log('listening on 8080'));
