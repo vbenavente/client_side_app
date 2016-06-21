1851eae93c9b0af759145b49e4ad7c3d7a35258e
Victor Benavente <VMB@Victors-MacBook-Air.local>
2016-06-20 23:53:14 -0700

    controllers for each resource added, started index.html

--- vic-class-24-assignment-clientsideapp/app/js/icecream/controllers/icecream-controller.js
+++ vic-class-24-assignment-clientsideapp/app/js/icecream/controllers/icecream-controller.js
@@ -1,1 +1,42 @@
-module.exports = function
+module.exports = function(app) {
+  app.controller('IceCreamController', IceCreamController);
+};
+
+function IceCreamController($http) {
+  this.$http = $http;
+  this.icecream = [];
+}
+
+IceCreamController.prototype.getIceCream = function() {
+  this.$http.get('http:/localhost:3000/icecream')
+  .then((res) => {
+    this.icecream = res.data;
+  }, (err) => {
+    console.log(err);
+  });
+};
+
+IceCreamController.prototype.addIceCream = function() {
+  this.$http.post('http//localhost:3000/', this.newIceCream)
+  .then((res) => {
+    this.icecream.push(res.data);
+    this.newIceCream = null;
+  }, (err) => {
+    console.log(err);
+  });
+};
+
+IceCreamController.prototype.updateIceCream = function(icecream, updatedIceCream) {
+  let arrayIceCream = this.icecream[this.icecream.indexOf(icecream)];
+  arrayIceCream.body = updatedIceCream;
+};
+
+IceCreamController.prototype.deleteIceCream = function(icecream) {
+  this.$http.delete('http//localhost:3000/' + icecream._id)
+  .then(() => {
+    let index = this.icecream.indexOf(icecream);
+    this.icecream.splice(index, 1);
+  }, (err) => {
+    console.log(err);
+  });
+};
--- vic-class-24-assignment-clientsideapp/app/js/milkshake/controllers/milkshake-controller.js
+++ vic-class-24-assignment-clientsideapp/app/js/milkshake/controllers/milkshake-controller.js
@@ -1,3 +1,42 @@
 module.exports = function(app) {
-  require('./controllers/milkshake-controller')(app);
+  app.controller('MilkShakeController', MilkShakeController);
+};
+
+function MilkShakeController($http) {
+  this.$http = $http;
+  this.milkshake = [];
+}
+
+MilkShakeController.prototype.getMilkShake = function() {
+  this.$http.get('http:/localhost:3000/milkshake')
+  .then((res) => {
+    this.milkshake = res.data;
+  }, (err) => {
+    console.log(err);
+  });
+};
+
+MilkShakeController.prototype.addMilkShake = function() {
+  this.$http.post('http//localhost:3000/', this.newMilkShake)
+  .then((res) => {
+    this.milkshake.push(res.data);
+    this.newMilkShake = null;
+  }, (err) => {
+    console.log(err);
+  });
+};
+
+MilkShakeController.prototype.updateMilkShake = function(milkshake, updatedMilkShake) {
+  let arrayMilkShake = this.milkshake[this.milkshake.indexOf(milkshake)];
+  arrayMilkShake.body = updatedMilkShake;
+};
+
+MilkShakeController.prototype.deleteMilkShake = function(milkshake) {
+  this.$http.delete('http//localhost:3000/' + milkshake._id)
+  .then(() => {
+    let index = this.milkshake.indexOf(milkshake);
+    this.milkshake.splice(index, 1);
+  }, (err) => {
+    console.log(err);
+  });
 };
