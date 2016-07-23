61cce9025ff90cac1eb1784caef175599f752c91
Victor Benavente <vbenavente@hotmail.com>
2016-07-22 21:00:51 -0700

    added app.config and broke out home into milkshake or icecream

--- vic-class-24-assignment-clientsideapp/app/js/client.js
+++ vic-class-24-assignment-clientsideapp/app/js/client.js
@@ -7,3 +7,26 @@ const IceCreameryApp = angular.module('IceCreameryApp', [ngRoute]);
 require('./icecream/icecream')(IceCreameryApp);
 require('./milkshake/milkshake')(IceCreameryApp);
 require('./tworesource/tworesource')(IceCreameryApp);
+
+IceCreameryApp.config(['$routeProvider', function($route) {
+  $route.when('/', {
+    templateUrl: './templates/partials/icecream.html',
+    controller: 'IceCreamController',
+    controllerAs: 'icctrl'
+  })
+  .when('/milkshake', {
+    templateUrl: './templates/partials/milkshake.html',
+    controller: 'MilkShakeController',
+    controllerAs: 'msctrl'
+  })
+  .when('/signin', {
+    templateUrl: './templates/partials/signin.html',
+    controller: 'SigninController',
+    controllerAs: 'signinctrl'
+  })
+  .when('/signup', {
+    templateUrl: './templates/partials/signup.html',
+    controller: 'SigninController',
+    controllerAs: 'signinctrl'
+  });
+}]);
